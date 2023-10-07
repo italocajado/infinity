2c5cdcd08f029a025448be44e14847702db1114c
italocajado <italocajado@gmail.com>
2023-10-07 20:58:34 -0300

    Add files via upload

---
+++ INTAlg/0710.py
@@ -0,0 +1,10 @@
+#função print
+
+nome =  (input(str("qual o seu nome? ")))
+
+print ("hello ", nome)
+
+num1 = int(input("digite um valor: "))
+num2 = int(input("digite outro numero: "))
+
+print ("a soma deu: ", num1+num2 )
