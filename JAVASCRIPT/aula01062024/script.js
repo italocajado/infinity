3105a270daba74e764c5a173a04f3cdad451c412
italocajado <italocajado@gmail.com>
2024-06-01 21:01:05 -0300

    Add files via upload

---
+++ JAVASCRIPT/aula01062024/script.js
@@ -0,0 +1,28 @@
+let nome = prompt("Qual o seu nome ? ")
+
+console.log('Olá, ' + nome)
+
+let hab = true
+let alt = 1.70
+
+if(hab == true){
+    const habV = console.log("Ele tem habilitação ? Tem sim")
+} else{
+    console.log("Ele tem habilitação ? Não tem")
+}
+
+
+console.log("Altura: " + alt + 'M')
+
+let num1 = Number(prompt("Digite o primeiro valor "))
+let num2 = Number(prompt("Digite o segundo valor "))
+
+let num5 = 5
+let sum = num5 + num1
+
+let divisiao = num1 / num2
+let mult = num1 * num2
+
+console.log(divisiao)
+console.log(mult)
+console.log(sum)
